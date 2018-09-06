244073ac7eb05a9c585c01ecd01112ddd219e19f
Refath Wadood <refath@sigscale.org>
2018-09-06 13:41:42 +0530

    add wait for alarm table function

--- src/snmp_collector_app.erl
+++ src/snmp_collector_app.erl
@@ -16,8 +16,8 @@
 %%% limitations under the License.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% @doc This {@link //stdlib/application. application} behaviour callback
-%%% 	module starts and stops the
-%%% 	{@link //sigcale_snmp_collector. sigscale_snmp_collector} application.
+%%%   module starts and stops the
+%%%   {@link //sigcale_snmp_collector. sigscale_snmp_collector} application.
 %%%
 -module(snmp_collector_app).
 -copyright('Copyright (c) 2018 SigScale Global Inc.').
@@ -33,15 +33,9 @@
 
 -include("snmp_collector.hrl").
 
--record(state, {}).
+-include_lib("sigscale_fm/include/fm.hrl").
 
--record(alarm,
-      {id :: {ReportingEntity :: string(), ID :: string()},
-      event_id :: string(),
-      source_id :: string(),
-      raised :: pos_integer(),
-      updated :: pos_integer(),
-      fault_fields :: map()}).
+-record(state, {}).
 
 %%----------------------------------------------------------------------
 %%  The snmp_collector_app aplication callbacks
@@ -60,6 +54,23 @@
 %% @see //kernel/application:start/2
 %%
 start(normal = _StartType, _Args) ->
+	Tables = [alarm],
+	case mnesia:wait_for_tables(Tables, 60000) of
+		ok ->
+			start1(normal = _StartType, []);
+		{timeout, BadTabList} ->
+			case force(BadTabList) of
+				ok ->
+					start1(normal = _StartType, []);
+				{error, Reason} ->
+					error_logger:error_report(["snmp_collector application failed to start",
+							{reason, Reason}, {module, ?MODULE}]),
+					{error, Reason}
+			end;
+		{error, Reason} ->
+			{error, Reason}
+   end.
+start1(_StartType, _Args) ->
 	{ok, Name} = application:get_env(queue_name),
 	{ok, Type} = application:get_env(queue_type),
 	{ok, Size} = application:get_env(queue_size),
@@ -73,14 +84,8 @@ start(normal = _StartType, _Args) ->
 					case timer:apply_interval(?INTERVAL, supervisor,
 							start_child, [snmp_collector_get_sup, [[], []]]) of
 						{ok, _TRef} ->
-							case mnesia:create_table(alarm,
-									[{attributes, record_info(fields, alarm)},
-									{ram_copies, [node()]}]) of
-								{atomic, ok} ->
-									{ok, PID};
-								{aborted, Reason} ->
-									{aborted, Reason}
-							end;
+							
+							{ok, PID};
 						{error, Reason} ->
 							{error, Reason}
 					end;
@@ -99,7 +104,7 @@ start(normal = _StartType, _Args) ->
 		Result :: ok | {error, Reason},
 		Reason :: term().
 %% @doc Called for each start phase in the application and included
-%% 	applications.
+%%   applications.
 %% @see //kernel/app
 %%
 start_phase(_Phase, _StartType, _PhaseArgs) ->
@@ -109,7 +114,7 @@ start_phase(_Phase, _StartType, _PhaseArgs) ->
 	when
 		State :: #state{}.
 %% @doc Called when the application is about to be shut down,
-%% 	before any processes are terminated.
+%%   before any processes are terminated.
 %% @see //kernel/application:stop/1
 %%
 prep_stop(State) ->
@@ -131,7 +136,7 @@ stop(_State) ->
 		Par :: atom(),
 		Val :: atom().
 %% @doc Called after a code  replacement, if there are any
-%% 	changes to the configuration  parameters.
+%%   changes to the configuration  parameters.
 %%
 config_change(_Changed, _New, _Removed) ->
 	ok.
@@ -141,14 +146,14 @@ config_change(_Changed, _New, _Removed) ->
 %%----------------------------------------------------------------------
 
 -spec open_log(Dir, Name, Type ,File, Size) -> Result
-   when
-      Dir :: string(),
-      Name :: atom(),
-      Type :: atom(),
-      File :: integer(),
-      Size :: integer(),
-      Result :: ok | {error, Reason},
-      Reason :: term().
+	when
+		Dir :: string(),
+		Name :: atom(),
+		Type :: atom(),
+		File :: integer(),
+		Size :: integer(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
 %% @doc Open a disk log file.
 open_log(Dir, Name, Type ,File, Size) ->
 	case file:make_dir(Dir) of
@@ -158,7 +163,7 @@ open_log(Dir, Name, Type ,File, Size) ->
 			open_log1(Dir, Name, Type ,File, Size);
 		{error, Reason} ->
 			{error, Reason}
-   end.
+	end.
 open_log1(Dir, Name, Type ,File, Size) ->
 	FileName = Dir ++ "/" ++ atom_to_list(Name),
 	case disk_log:open([{name, Name},
@@ -172,7 +177,7 @@ open_log1(Dir, Name, Type ,File, Size) ->
 			open_log2(Name, [], [{node(), Result}], undefined);
 		{OkNodes, ErrNodes} ->
 			open_log2(Name, OkNodes, ErrNodes, undefined)
-   end.
+	end.
 %% @hidden
 open_log2(Name, OkNodes,
 		[{Node, {error, {node_already_open, _}}} | T], Reason)
@@ -194,3 +199,19 @@ open_log2(_Name, OkNodes, [], Reason) ->
 			{error, Reason}
 	end.
 
+-spec force(Tables) -> Result
+	when
+		Tables :: [TableName],
+		Result :: ok | {error, Reason},
+		TableName :: atom(),
+		Reason :: term().
+%% @doc Try to force load bad tables.
+force([H | T]) ->
+	case mnesia:force_load_table(H) of
+		yes ->
+			force(T);
+		ErrorDescription ->
+			{error, ErrorDescription}
+	end;
+force([]) ->
+	ok.
