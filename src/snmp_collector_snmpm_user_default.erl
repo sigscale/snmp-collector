149a504201850106ac0ddf84e65d1ac99fad43fc
Vance Shipley <vances@sigscale.org>
2018-10-02 15:04:39 +0530

    default snmpm_user callback

---
+++ src/snmp_collector_snmpm_user_default.erl
@@ -0,0 +1,206 @@
+%%% snmp_collector_snmpm_user_default.erl
+%%% vim: ts=3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2018 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+-module(snmp_collector_snmpm_user_default).
+-copyright('Copyright (c) 2018 SigScale Global Inc.').
+
+-export([handle_error/3, handle_agent/5, handle_pdu/4,
+		handle_trap/3, handle_inform/3, handle_report/3]).
+
+-behaviour(snmpm_user).
+
+%%----------------------------------------------------------------------
+%%  The snmp_user callbacks
+%%----------------------------------------------------------------------
+
+-spec handle_error(ReqId, Reason, UserData) -> any()
+	when
+		ReqId :: integer(),
+		Reason :: {unexpected_pdu, SnmpInfo}
+				| {invalid_sec_info, SecurityInfo, SnmpInfo}
+				| {empty_message, Address, Port} | term(),
+		SnmpInfo :: snmpm_user:snmp_gen_info(),
+		SecurityInfo :: term(),
+		Address :: inet:ip_address(),
+		Port :: inet:port()
+		UserData :: term().
+%% @doc Called when the manager needs to communicate an "asynchronous"
+%% 	error to the user.
+handle_error(ReqId,
+		{unexpected_pdu, {Status, Index, VarBinds}} = _Reason, UserData) ->
+	error_logger:error_report(["SNMP Manager: Error",
+			{reqid, ReqId}, {error, unexpected_pdu},
+			{status, Status}, {index, Index},
+			{varbinds, VarBinds}, {userdata, UserData}]);
+handle_error(ReqId,
+		{invalid_sec_info, SecurityInfo, {Status, Index, VarBinds}}, UserData) ->
+	error_logger:error_report(["SNMP Manager: Error",
+			{reqid, ReqId}, {error, invalid_sec_info},
+			{sec_info, SecurityInfo}, {status, Status},
+			{index, Index}, {varbinds, VarBinds},
+			{userdata, UserData}]);
+handle_error(ReqId, {empty_message, Address, Port}, UserData) ->
+	error_logger:error_report(["SNMP Manager: Error",
+			{reqid, ReqId}, {error, empty_message},
+			{address, Address}, {port, Port},
+			{userdata, UserData}]);
+handle_error(ReqId, Other, UserData) ->
+	error_logger:error_report(["SNMP Manager: Error",
+			{reqid, ReqId}, {error, Other},
+			{userdata, UserData}]);
+
+-spec handle_agent(Domain, Address, Type, SnmpInfo, UserData) -> Reply
+	when
+		Domain :: transportDomainUdpIpv4 | transportDomainUdpIpv6,
+		Address :: {inet:ip_address(), inet:port_number()},
+		Type :: pdu | trap | report | inform,
+		SnmpInfo :: SnmpPduInfo | SnmpTrapInfo | SnmpReportInfo | SnmpInformInfo,
+		SnmpPduInfo :: snmpm_user:snmp_gen_info(),
+		SnmpTrapInfo :: snmpm_user:snmp_v1_trap_info(),
+		SnmpReportInfo :: snmpm_user:snmp_gen_info(),
+		SnmpInformInfo :: snmpm_user:snmp_gen_info(),
+		UserData :: term(),
+		Reply :: ignore | {register, UserId, TargetName, AgentConfig}
+		UserId :: term(),
+		TargetName :: snmpm:target_name(),
+		AgentConfig :: [snmpm:agent_config()].
+%% @doc Called when a message is received from an unknown agent.
+handle_agent(_Domain, {IpAddress, Port} = Address, Type,
+		{Enteprise, Generic, Spec, Timestamp, Varbinds}, UserData) ->
+	error_logger:error_report(["SNMP Manager: Unknown Agent",
+			{address, IpAddress}, {port, Port}, (type, Type},
+			{enterprise, Enteprise}, {generic, Generic}, {spec, Spec},
+			{timestamp, Timestamp}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    ignore;
+handle_agent(_Domain, {IpAddress, Port} = Address, Type,
+		{Status, Index, Varbinds} = _SnmpInfo, UserData) ->
+	error_logger:error_report(["SNMP Manager: Unknown Agent",
+			{address, IpAddress}, {port, Port}, (type, Type},
+			{status, Status}, {index, Index}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    ignore.
+
+-spec handle_pdu(TargetName, ReqId, SnmpPduInfo, UserData) -> any()
+	when
+		TargetName :: snmpm:target_name(),
+		ReqId :: term(),
+		SnmpPduInfo :: snmp_user:snmp_gen_info(),
+		UserData :: term().
+%% @doc Handle the reply to an asynchronous request (e.g. `snmpm:async_get/3').
+handle_pdu(TargetName, ReqId,
+		{Status, Index, Varbinds} = _SnmpPduInfo, UserData) ->
+	error_logger:error_report(["SNMP Manager: PDU Reply",
+			{target_name, TargetName}, {reqid, ReqId},
+			{status, Status}, {index, Index}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    ignore.
+
+-spec handle_trap(TargetName, SnmpTrapInfo, UserData) -> Reply
+	when
+		TargetName :: snmpm:target_name(),
+		SnmpTrapInfo :: snmpm_user:snmp_v1_trap_info()
+				| snmpm_user:snmp_gen_info(),
+		UserData :: term(),
+		Reply :: ignore | unregister
+				| {register, UserId, TargetName, AgentConfig},
+		UserId :: term(),
+		AgentConfig :: [snmpm:agent_config()].
+%% @doc Handle a trap/notification message from an agent.
+handle_trap(TargetName,
+		{Status, Index, Varbinds} = _SnmpTrapInfo, UserData) ->
+	error_logger:error_report(["SNMP Manager: Received Trap",
+			{target_name, TargetName},
+			{status, Status}, {index, Index}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    ignore;
+handle_trap(TargetName,
+		 {Enteprise, Generic, Spec, Timestamp, Varbinds}, UserData) ->
+	error_logger:error_report(["SNMP Manager: Received Trap",
+			{target_name, TargetName},
+			{enterprise, Enteprise}, {generic, Generic}, {spec, Spec},
+			{timestamp, Timestamp}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    ignore.
+
+-spec handle_inform(TargetName, SnmpInformInfo, UserData) -> Reply
+	when
+		TargetName :: snmpm:target_name(),
+		SnmpInformInfo :: snmpm_user:snmp_gen_info(),
+		UserData :: term(),
+		Reply :: ignore | no_reply | unregister
+				| {register, UserId, TargetName, AgentConfig},
+		UserId :: term(),
+		AgentConfig :: [snmpm:agent_config()].
+%% @doc Handle an inform message.
+handle_inform(TargetName,
+		{Status, Index, Varbinds} = _SnmpInformInfo, UserData) ->
+	error_logger:error_report(["SNMP Manager: Received Inform",
+			{target_name, TargetName},
+			{status, Status}, {index, Index}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    no_reply.
+
+-spec handle_report(TargetName, SnmpReportInfo, UserData) -> Reply
+	when
+		TargetName :: snmpm:target_name(),
+		Address :: inet:ip_address(),
+		Port :: inet:port(),
+		SnmpReportInfo :: snmpm_user:snmp_gen_info(),
+		UserData :: term(),
+		Reply :: ignore | unregister
+				| {register, UserId, TargetName, AgentConfig},
+		UserId :: term(),
+		AgentConfig :: [snmpm:agent_config()].
+%% @doc Handle a report message.
+handle_report(TargetName,
+		{Status, Index, Varbinds} = _SnmpReportInfo, UserData) ->
+	error_logger:error_report(["SNMP Manager: Received Report",
+			{target_name, TargetName},
+			{status, Status}, {index, Index}, {varbinds, VarBinds},
+			{userdata, UserData}]),
+    ignore.
+
+spec handle_invalid_result(In, Out) -> any()
+	when
+		In :: {Function, Args},
+		Function :: atom()
+		Args :: list(),
+		Out :: {crash, CrashInfo} | {result, InvalidResult},
+		CrashInfo :: {ErrorType, Error, Stacktrace},
+		ErrorType :: atom(),
+		Error :: term(),
+		Stacktrace :: list(),
+		InvalidResult :: term().
+%% @doc Called if any of the other callback functions crashes.
+handle_invalid_result({Function, Args} = _In,
+		{crash, {ErrorType, Error, Stacktrace} = _CrashInfo} = _Out) ->
+	error_logger:error_report(["SNMP Manager: User Callback Crashed",
+			{module, ?MODULE}, {function, Function}, {args, Args},
+			{error_type, ErrorType}, {error, Error},
+			{stacktrace, Stacktrace}]);
+handle_invalid_result({Function, Args} = _In,
+		{result, Result} = _Out) ->
+	error_logger:error_report(["SNMP Manager: User Callback Invalid Result",
+			{module, ?MODULE}, {function, Function}, {args, Args},
+			{result, Result}]).
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+
