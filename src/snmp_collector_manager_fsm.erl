318a67ec63550e0809121492d93a58a52eeadb10
Refath Wadood <refath@sigscale.org>
2019-01-04 13:29:33 +0530

    refactor handle pdu function

--- src/snmp_collector_manager_fsm.erl
+++ src/snmp_collector_manager_fsm.erl
@@ -26,14 +26,18 @@
 -export([init/1, terminate/3]).
 -export([code_change/4, handle_event/3 ,handle_info/3 ,handle_sync_event/4]).
 %% export the gen fsm states.
--export([decode/2]).
-
+-export([handle_pdu/2]).
+% add handle_trap/2
 -record(statedata, {socket :: inet:socket() | undefined,
 		address :: inet:ip_address() | undefined,
 		port :: non_neg_integer() | undefined,
 		packet :: [byte()] | undefined}).
 
+-define(SNMP_USE_V3, true).
+-define(VMODULE,"USM").
+
 -include_lib("snmp/include/snmp_types.hrl").
+-include_lib("../../snmp-collector/include/snmp_collector.hrl").
 
 %%----------------------------------------------------------------------
 %%  The call back functions
@@ -53,11 +57,11 @@
 %% @private
 %%
 init([Socket, Address, Port, Packet]) ->
-	{ok, decode, #statedata{socket = Socket,
+	{ok, handle_pdu, #statedata{socket = Socket,
 			address = Address, port = Port,
 			packet = Packet}, 0}.
 
--spec decode(Event, StateData) -> Result
+-spec handle_pdu(Event, StateData) -> Result
 	when
 		Event :: timeout | term(),
 		StateData :: #statedata{},
@@ -68,17 +72,129 @@ init([Socket, Address, Port, Packet]) ->
 		NextStateName :: atom(),
 		NewStateData :: #statedata{},
 		Timeout :: non_neg_integer() | infinity,
-		Reason :: normal | term().
+		Reason :: normal | shutdown | term().
 %% @doc Handle events sent with {@link //stdlib/gen_fsm:send_event/2.
-%%		gen_fsm:send_event/2} in the <b>send_request</b> state. This state is responsible
-%%		for sending a RADIUS-Disconnect/Request to an access point.
+%%		gen_fsm:send_event/2} in the <b>send_request</b> state.
 %% @@see //stdlib/gen_fsm:StateName/2
 %% @private
 %%
-decode(_Event, #statedata{socket = Socket, address = Address,
+handle_pdu(timeout = _Event, #statedata{socket = _Socket, address = Address,
 		port = Port, packet = Packet} = StateData) ->
-	decode(Socket, Address, Port, Packet),
-	{next_state, decode, StateData, 0}.
+	case catch snmp_pdus:dec_message_only(Packet) of
+		#message{version = 'version-1'} ->
+			{stop, shutdown, StateData};
+		#message{version = 'version-2'} ->
+			{stop, shutdown, StateData};
+		#message{version = 'version-3', vsn_hdr = #v3_hdr{msgSecurityParameters = SecurityParams, msgFlags = [Flag]}, data = Data} ->
+			case catch snmp_pdus:dec_usm_security_parameters(SecurityParams) of
+				#usmSecurityParameters{msgUserName = UserName, msgAuthoritativeEngineID = EngineID, msgAuthoritativeEngineBoots = EngineBoots,
+						msgAuthoritativeEngineTime = EngineTime, msgPrivacyParameters = MsgPrivParams, msgAuthenticationParameters = MsgAuthenticationParams} ->
+					Fsearch = fun() ->
+								mnesia:read(snmp_users, UserName, read)
+					end,
+					case mnesia:ets(Fsearch) of
+						[{snmp_users, _, AuthPass, PrivPass}] ->
+							case catch snmp_pdus:dec_scoped_pdu_data(Data) of
+								PDU when is_list(PDU) ->
+									case snmp_collector_utils:security_params(EngineID, UserName, MsgAuthenticationParams,
+											Packet, AuthPass, PrivPass) of
+										{ok, usmNoAuthProtocol, usmNoPrivProtocol} when Flag == 0 ->
+											case handle_trap(Address, Port, {noError, 0, Data}) of
+												ignore ->
+													{stop, shutdown, StateData};
+												{error, Reason} ->
+													error_logger:info_report(["SNMP Manager Agent Not Recognized",
+															{error, Reason},
+															{engine_id, EngineID},
+															{username, UserName},
+															{flags, authPriv},
+															{port, Port},
+															{address, Address}]),
+														{stop, shutdown, StateData}
+											end;
+										{ok, usmHMACMD5AuthProtocol, usmNoPrivProtocol} when Flag == 1 ->
+											{stop, shutdown, StateData};
+										{ok, usmHMACMD5AuthProtocol, usmDESPrivProtocol} when Flag == 3 ->
+											PrivKey = snmp:passwd2localized_key(md5, PrivPass, EngineID),
+											case dec_des(PrivKey, MsgPrivParams, PDU) of
+												{ErrorStatus, ErrorIndex, Varbinds} ->
+													case handle_trap(Address, Port, {ErrorStatus, ErrorIndex, Varbinds}) of
+														ignore ->
+															io:write(works),
+															{stop, shutdown, StateData};
+														{error, Reason} ->
+															error_logger:info_report(["SNMP Manager Agent Not Recognized",
+																	{error, Reason},
+																	{engine_id, EngineID},
+																	{username, UserName},
+																	{flags, authPriv},
+																	{port, Port},
+																	{address, Address}]),
+															{stop, shutdown, StateData}
+													end;
+												{error, Reason} ->
+													{error, Reason}
+											end;
+										{ok, usmHMACMD5AuthProtocol, usmAesCfb128Protocol} when Flag == 3 ->
+											{stop, shutdown, StateData};
+										{ok, usmHMACSHAAuthProtocol, usmNoPrivProtocol} when Flag == 1 ->
+											{stop, shutdown, StateData};
+										{ok, usmHMACSHAAuthProtocol, usmDESPrivProtocol} when Flag == 3 ->
+											PrivKey = snmp:passwd2localized_key(sha, PrivPass, EngineID),
+											case dec_aes(PrivKey, MsgPrivParams, Data, EngineBoots, EngineTime) of
+												{ErrorStatus, ErrorIndex, Varbinds} ->
+													case handle_trap(Address, Port, {ErrorStatus, ErrorIndex, Varbinds}) of
+														ignore ->
+															{stop, shutdown, StateData};
+														{error, Reason} ->
+															error_logger:info_report(["SNMP Manager Agent Not Found",
+																	{error, Reason},
+																	{username, UserName},
+																	{port, Port},
+																	{address, Address}]),
+															{stop, shutdown, StateData}
+													end;
+												{error, Reason} ->
+													{error, Reason}
+											end;
+										{ok, usmHMACSHAAuthProtocol, usmAesCfb128Protocol} when Flag == 3 ->
+											{stop, shutdown, StateData};
+										{error, Reason} ->
+											error_logger:info_report(["SNMP Manager Incorrect Security Params",
+													{error, Reason},
+													{username, UserName},
+													{port, Port},
+													{address, Address}]),
+											{stop, shutdown, StateData}
+									end;
+								{'EXIT', Reason} ->
+									error_logger:info_report(["SNMP Manager Decoding SNMP v3 Failed",
+											{reason, Reason},
+											{port, Port},
+											{address, Address}]),
+										{stop, shutdown, StateData}
+							end;
+						_ ->
+							error_logger:info_report(["SNMP Manager User Not Found",
+									{username, UserName},
+									{port, Port},
+									{address, Address}]),
+							{stop, shutdown, StateData}
+					end;
+				{'EXIT', Reason} ->
+					error_logger:info_report(["SNMP Manager Decoding SNMP v3 Failed",
+							{reason, Reason},
+							{port, Port},
+							{address, Address}]),
+							{stop, shutdown, StateData}
+		end;
+		{'EXIT', Reason} ->
+			error_logger:info_report(["SNMP Manager Decoding SNMP v3 Failed",
+					{reason, Reason},
+					{port, Port},
+					{address, Address}]),
+					{stop, shutdown, StateData}
+	end.
 
 -spec handle_event(Event, StateName, StateData) -> Result
 	when
@@ -172,61 +288,129 @@ code_change(_OldVsn, StateName, StateData, _Extra) ->
 %%  The internal functions
 %%----------------------------------------------------------------------
 
--spec decode(Socket, Address, Port, Packet) -> Result
+-spec dec_des(PrivKey, MsgPrivParams, Data) -> Result
 	when
-		Socket :: inet:socket() | undefined,
-		Address :: inet:ip_address() | undefined,
-		Port :: non_neg_integer() | undefined,
-		Packet :: [byte()]| undefined,
-		Result :: Varbinds :: [tuple()] | ok.
-%% @doc Decode the packet.
-decode(Socket,Address, Port, Packet) ->
-	case catch snmp_pdus:dec_message(Packet) of
-		#message{version = 'version-1',
-				data = Data} ->
-			decode_v1_v2(v1, Data);
-		#message{version = 'version-2',
-				data = Data} ->
-			decode_v1_v2(v2c, Data);
-		Message = #message{version = 'version-3',
-				data = Data} ->
-			decode_v3(v3, Message, Data);
-		{'EXIT', Reason} ->
-			error_logger:info_report(["SNMP Manager",
-					{reason, Reason},
-					{socket, Socket},
-					{address, Address},
-					{port, Port}])
+		PrivKey :: list(),
+		MsgPrivParams :: list(),
+		Data :: [byte()],
+		Result :: {ErrorStatus, ErrorIndex, Varbinds} | {error, Reason},
+		ErrorStatus :: atom(),
+		ErrorIndex :: integer(),
+		Varbinds :: [snmp:varbinds()],
+		Reason :: des_decryption_failed | term().
+%% @doc Decrypt a SNMP packet using DES privacy protocol.
+dec_des(PrivKey, MsgPrivParams, Data
+	when is_list(PrivKey), is_list(MsgPrivParams) ->
+	case catch snmp_usm:des_decrypt(PrivKey, MsgPrivParams, Data) of
+		{ok, DecryptedData} ->
+			case snmp_pdus:dec_scoped_pdu_data(DecryptedData) of
+				#scopedPdu{data = #pdu{error_status = ErrorStatus,
+						error_index = ErrorIndex, varbinds = Varbinds}} ->
+					{ErrorStatus, ErrorIndex, Varbinds};
+				ScopedPDU ->
+					case snmp_pdus:dec_scoped_pdu(ScopedPDU) of
+						#scopedPdu{data = #pdu{error_status = ErrorStatus,
+								error_index = ErrorIndex, varbinds = Varbinds}} ->
+							{ErrorStatus, ErrorIndex, Varbinds};
+						_ ->
+							{error, des_decryption_failed}
+					end
+			end;
+		{'EXIT', Reason}->
+			{error, Reason}
 	end.
 
--spec decode_v1_v2(Version, Data) -> Result
+-spec dec_aes(PrivKey, MsgPrivParams, Data, EngineBoots, EngineTime) -> Result
 	when
-		Version :: v1 | v2c,
-		Data :: #pdu{} | #scopedPdu{},
-		Result :: ok.
-%% @doc Decode the data from a SNMP  'v1' or 'v2c' Packet.
-decode_v1_v2(Version, #pdu{varbinds = Varbinds} = Data) ->
-	ok;
-decode_v1_v2(Version, #scopedPdu{data = Varbinds} = Data) ->
-	ok.
-	
+		PrivKey :: list(),
+		MsgPrivParams :: list(),
+		Data :: [byte()],
+		EngineBoots :: integer(),
+		EngineTime :: integer(),
+		Result :: {ErrorStatus, ErrorIndex, Varbinds} | {error, Reason},
+		ErrorStatus :: atom(),
+		ErrorIndex :: integer(),
+		Varbinds :: [snmp:varbinds()],
+		Reason :: aes_decryption_failed | term().
+%% @doc Decrypt a SNMP packet data using DES privacy protocol.
+dec_aes(PrivKey, MsgPrivParams, Data, EngineBoots, EngineTime) ->
+	case catch snmp_usm:aes_decrypt(PrivKey, MsgPrivParams, Data, EngineBoots, EngineTime) of
+		{ok, DecryptedData} ->
+			case snmp_pdus:dec_scoped_pdu_data(DecryptedData) of
+				#scopedPdu{data = #pdu{error_status = ErrorStatus,
+						error_index = ErrorIndex, varbinds = Varbinds}} ->
+					{ErrorStatus, ErrorIndex, Varbinds};
+				ScopedPDU ->
+					case snmp_pdus:dec_scoped_pdu(ScopedPDU) of
+						#scopedPdu{data = #pdu{error_status = ErrorStatus,
+								error_index = ErrorIndex, varbinds = Varbinds}} ->
+							{ErrorStatus, ErrorIndex, Varbinds};
+						_ ->
+							{error, aes_decryption_failed}
+					end
+			end;
+		{'EXIT', Reason}->
+			{error, Reason}
+	end.
 
--spec decode_v3(Message, Version, Data) -> Result
+-spec handle_trap(Address, Port, TrapInfo) -> Result
 	when
-		Message :: #message{},
-		Version :: v3,
-		Data :: #pdu{} | #scopedPdu{},
-		Result :: ok | {'EXIT', Reason},
+		Address :: inet:ip_address(),
+		Port :: pos_integer(),
+		TrapInfo :: {ErrorStatus, ErrorIndex, Varbinds},
+		ErrorStatus :: atom(),
+		ErrorIndex :: integer(),
+		Varbinds :: [snmp:varbinds()],
+		Result :: ok | {error, Reason},
 		Reason :: term().
-%% @doc Decode the data from a SNMP 'v3' Packet.
-decode_v3(Version, #message{community = V3Header} = Message, Data) ->
-	DecodedPacket = V3Header#v3_hdr.msgSecurityParameters,
-	case catch snmp_pdus:dec_usm_security_parameters(DecodedPacket) of
-		#usmSecurityParameters{msgUserName = UserName,
-				msgAuthoritativeEngineID = EngineID} ->
-			[{{EntityName, engine_id}, _}] = ets:lookup(snmpm_agent_table, {UserName, engine_id}),
-			ok;
-		{'EXIT', Reason} ->
-			{'EXIT', Reason}
+%% @doc Send Varbinds to the associated trap handler modules.
+handle_trap(Address, Port, {ErrorStatus, ErrorIndex, Varbinds})
+		when ErrorStatus == noError ->
+	case agent_name(Address, Port) of
+		{"huawei", TargetName} ->
+			snmp_collector_huawei_trap:handle_trap(TargetName, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"zte", TargetName} ->
+			snmp_collector_zte_trap:handle_trap(TargetName, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"nec", TargetName} ->
+			snmp_collector_nec_trap:handle_trap(TargetName, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"nokia", TargetName} ->
+			snmp_collector_nokia_trap:handle_trap(TargetName, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"huawei-datacom", TargetName} ->
+			snmp_collector_huawei_data_com_trap:handle_trap(TargetName, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"huawei-optical", TargetName} ->
+			snmp_collector_huawei_data_com_trap:handle_trap(TargetName, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"emc", _TargetName} ->
+			snmp_collector_snmpm_user_default:handle_agent(transportDomainUdpIpv4, {Address, Port},
+					trap, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{"hp", _TargetName} ->
+			snmp_collector_snmpm_user_default:handle_agent(transportDomainUdpIpv4, {Address, Port},
+					trap, {ErrorStatus, ErrorIndex, Varbinds}, []);
+		{error, Reason} ->
+		snmp_collector_snmpm_user_default:handle_agent(transportDomainUdpIpv4, {Address, Port},
+				trap, {ErrorStatus, ErrorIndex, Varbinds}, []),
+			{error, Reason}
+	end.
+
+-spec agent_name(Address, Port) -> Result
+	when
+		Address :: inet:ip_address(),
+		Port :: pos_integer(),
+		Result :: {AgentName, TargetName} | {error, Reason},
+		AgentName :: string(),
+		TargetName :: snmpm:target_name(),
+		Reason :: target_name_not_found | agent_name_not_found | term().
+%% @doc Identify the Agent Name for the received packet.
+agent_name(Address, Port) ->
+	case ets:match(snmpm_agent_table, {{'$1', '_'}, {Address ,Port}}) of
+		[[TargetName]] ->
+			case ets:match(snmpm_agent_table, {{TargetName, user_id},'$1'}) of
+				[[AgentName]] ->
+					{AgentName, TargetName};
+				[] ->
+					{error, agent_name_not_found}
+			end;
+		[] ->
+			{error, target_name_not_found}
 	end.
 	
+
