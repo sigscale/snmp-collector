d21056866e567ae51908c8ab9b83476996fb5081
Refath Wadood <refath@sigscale.org>
2019-01-16 17:07:22 +0530

    refactor the snmp collector mib module

--- src/snmp_collector_mib.erl
+++ src/snmp_collector_mib.erl
@@ -20,7 +20,7 @@
 -copyright('Copyright (c) 2016 - 2019 SigScale Global Inc.').
 
 %% export the snmp_collector_mib_loader public API
--export[load_default_mibs/0, load_mibs/0].
+-export[load_default_mibs/1, load_manager_mibs/2].
 
  -include_lib("kernel/include/file.hrl").
 
@@ -28,105 +28,108 @@
 %%  The snmp_collector_mib_loader public API
 %%----------------------------------------------------------------------
 
--spec load_default_mibs() -> Result
+-spec load_default_mibs(DefaultMibDir) -> Result
 	when
+		DefaultMibDir :: string(),
 		Result :: ok | {error, Reason},
 		Reason :: term().
 %% @doc Load default MIBs for the snmp collector.
-load_default_mibs() ->
-	{ok, Dir} = application:get_env(default_bin_dir, snmp_collector),
-	{ok, MibList} = file:list_dir(Dir),
-	load_default_mibs(Dir, MibList).
+load_default_mibs(DefaultMibDir) ->
+	{ok, MibList} = file:list_dir(DefaultMibDir),
+	load_default_mibs(DefaultMibDir, MibList).
 %% @hidden
 load_default_mibs(Dir, [Mib | Rest]) ->
-	MibPath = Dir ++ Mib,
+	MibPath = Dir ++ "/" ++ Mib,
 	case snmpm:load_mib(MibPath) of
 		ok ->
 			load_default_mibs(Dir, Rest);
 		{error, already_loaded} ->
+			load_default_mibs(Dir, Rest);
+		{error, Reason} ->
+			error_logger:info_msg(["SNMP MIB Failed To Load Mib",
+				{mib_name, Mib},
+				{error, Reason}]),
 			load_default_mibs(Dir, Rest)
 	end;
 load_default_mibs(_, []) ->
 	ok.
 
--spec load_mibs() -> Result
+-spec load_manager_mibs(MibDir, BinDir) -> Result
 	when
+		MibDir :: string(),
+		BinDir :: string(),
 		Result :: ok | {error, Reason},
 		Reason :: term().
 %% @doc Load all MIBs into the SNMP Manager.
-load_mibs() ->
-	case compile_mibs() of
-		ok ->
-			load_mibs1();
+load_manager_mibs(MibDir, BinDir) ->
+	case file:list_dir(MibDir) of
+		{ok, []} ->
+			{error, mib_directory_empty};
+		{ok, MibList} ->
+			case file:list_dir(MibDir) of
+				{ok, BinList} ->
+					case compile_mibs(MibDir, BinDir, MibList, BinList) of
+						ok ->
+							case load_mibs(BinDir) of
+								ok ->
+									ok;
+								{error, Reason} ->
+									{error, Reason}
+							end;
+						{error ,Reason} ->
+							{error, Reason}
+					end;
+				{error, Reason} ->
+					{error, Reason}
+			end;
 		{error, Reason} ->
-			error_logger:info_msg(["SNMP MIB Compilation Failed",
+			error_logger:info_msg(["SNMP Manager Failed To Load Mib Directory",
 				{error, Reason}]),
 			{error, Reason}
 	end.
-%% @hidden
-load_mibs1() ->
-	{ok, BinDir} = application:get_env(snmp_collector, bin_dir),
-	{ok, BinList} = file:list_dir(BinDir),
-	load_mibs2(BinDir, BinList).
-%% @hidden
-load_mibs2(BinDir, [BinFile | Rest]) ->
-	BinPath = BinDir ++ BinFile,
-	case snmpm:load_mib(BinPath) of
-		ok ->
-			load_default_mibs(BinDir, Rest);
-		{error, already_loaded} ->
-			load_default_mibs(BinDir, Rest)
-	end.
-
-
-	
 
 %%----------------------------------------------------------------------
 %%  The internal functions
 %%----------------------------------------------------------------------
 
--spec compile_mibs() -> Result
+-spec compile_mibs(MibDir, BinDir, MibList, BinList) -> Result
 	when
+		MibDir :: string(),
+		BinDir :: string(),
+		MibList :: list(),
+		BinList :: list(),
 		Result :: ok | {error, Reason},
 		Reason :: term().
 %% @doc Compile all MIBs in the Mib directory.
-compile_mibs() ->
-	{ok, MibDir} = application:get_env(mib_dir, snmp_collector),
-	{ok, BinDir} = application:get_env(bin_dir, snmp_collector),
-	{ok, MibList} = file:list_dir(MibDir),
-	{ok, BinList} = file:list_dir(BinDir),
-	compile_mibs(MibDir, BinDir ,MibList, BinList).
+compile_mibs(MibDir, BinDir, ["bin" | T], BinList) ->
+	compile_mibs(MibDir, BinDir, T, BinList);
 %% @hidden
 compile_mibs(MibDir, BinDir ,[Mib | T], BinList) ->
-	MibPath = MibDir ++ Mib,
+	MibPath = MibDir ++ "/" ++ Mib,
 	case file:read_file(MibPath) of
 		{ok, File} ->
-			MibName = snmp_collector_utils:get_name(binary_to_list(File)) ++ ".bin",
-			BinPath = BinDir ++ MibName,
-			case lists:member(MibName, BinList) of
+			BinName = snmp_collector_utils:get_name(binary_to_list(File)) ++ ".bin",
+			BinPath = BinDir ++ "/" ++ BinName,
+			case lists:member(BinName, BinList) of
 				true ->
 					case file:read_file_info(MibPath, [{time, local}]) of
 						{ok, #file_info{mtime = MibTimeStamp}} ->
 							case file:read_file_info(BinPath, [{time, local}]) of
 								{ok, #file_info{mtime = BinTimeStamp}} ->
-									case MibTimeStamp > BinTimeStamp	of 
+									case MibTimeStamp > BinTimeStamp of
 										true ->
-											case snmpc:compile(MibName, [module_identity,
-													{outdir, BinDir}, {group_check, false}]) of
+											case snmpc:compile(MibPath, [module_identity,
+													{outdir, BinDir}, {group_check, false}, {warnings, false},
+													{i, [BinDir]}, {il, ["snmp/priv/mibs/"]}]) of
 												{ok, BinFileName} ->
-													case load_mib(BinFileName) of
-														ok ->
-															error_logger:info_msg(["SNMP Collector MIB Compiled",
-																		{mib_name, MibName}]),
-															compile_mibs(MibDir, BinDir, T, BinList);
-														{error, Reason} ->
-															error_logger:error_msg(["SNMP Collector MIB Compilation Failed",
-																		{error, Reason},		
-																		{mib_name, MibName}]),
-															compile_mibs(MibDir, BinDir, T, BinList)
-													end;
+													error_logger:info_msg(["SNMP Manager Complied MIB",
+															{mib, BinFileName}]),
+													compile_mibs(MibDir, BinDir, T, BinList);
 												{error, Reason} ->
-													{error, Reason}
+													error_logger:warning_msg(["SNMP Collector MIB Compilation Failed",
+															{error, Reason},
+															{mib_name, Mib}]),
+													compile_mibs(MibDir, BinDir, T ++ [Mib], BinList)
 											end;
 										false ->
 											compile_mibs(MibDir, BinDir, T, BinList)
@@ -138,7 +141,19 @@ compile_mibs(MibDir, BinDir ,[Mib | T], BinList) ->
 							{error, Reason}
 					end;
 				false ->
-					compile_mibs(MibDir, BinDir, T, BinList)
+					case snmpc:compile(MibPath, [module_identity,
+						{outdir, BinDir}, {group_check, false}, {warnings, false},
+								{i, [BinDir]}, {il, ["snmo/priv/mibs/"]}]) of
+							{ok, BinFileName} ->
+								error_logger:info_msg(["SNMP Manager Complied MIB",
+										{mib, BinFileName}]),
+								compile_mibs(MibDir, BinDir, T, BinList);
+							{error, Reason} ->
+								error_logger:warning_msg(["SNMP Collector MIB Compilation Failed",
+										{error, Reason},
+										{mib_name, Mib}]),
+								compile_mibs(MibDir, BinDir, T ++ [Mib], BinList)
+					end
 			end;
 		{error, Reason} ->
 			{error, Reason}
@@ -147,17 +162,39 @@ compile_mibs(MibDir, BinDir ,[Mib | T], BinList) ->
 compile_mibs(_, _, [], _) ->
 	ok.
 
--spec load_mib(BinPath) -> Result
+-spec load_mibs(BinDir) -> Result
 	when
-		BinPath :: string(),
+		BinDir :: string(),
 		Result :: ok | {error, Reason},
 		Reason :: term().
 %% @doc Load a mib into the SNMP Manager.
-load_mib(BinPath) 
-		when is_list(BinPath) ->
-	case snmpm:load_mib(BinPath) of
-		ok ->
-			ok;
+load_mibs(BinDir)
+		when is_list(BinDir) ->
+	case file:list_dir(BinDir) of
+		{ok, BinList} ->
+			case load_mibs1(BinDir, BinList) of
+				ok ->
+					ok;
+				{error, Reason} ->
+					{error ,Reason}
+			end;
 		{error, Reason} ->
 			{error, Reason}
 	end.
+%% @hidden
+load_mibs1(BinDir, [BinFile | Rest]) ->
+	BinPath = BinDir ++ "/" ++ BinFile,
+	case snmpm:load_mib(BinPath) of
+		ok ->
+			load_mibs1(BinDir, Rest);
+		{error, {already_loaded, _, _}} ->
+			load_mibs1(BinDir, Rest);
+		{error, already_loaded} ->
+			load_mibs1(BinDir, Rest);
+		{error, Reason} ->
+			{error, Reason}
+	end;
+%% @hidden
+load_mibs1(_, []) ->
+	ok.
+
