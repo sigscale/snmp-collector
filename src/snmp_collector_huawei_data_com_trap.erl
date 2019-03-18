2dcfe0b61b331137cf019553477707f3636875b2
Refath Wadood <refath@sigscale.org>
2019-03-18 11:35:44 +0530

    fix typos

--- src/snmp_collector_huawei_data_com_trap.erl
+++ src/snmp_collector_huawei_data_com_trap.erl
@@ -99,17 +99,13 @@ handle_trap(TargetName, {_ErrorStatus, _ErrorIndex, Varbinds}, _UserData) ->
 		true ->
 			ignore;
 		false ->
-			case snmp_collector_utils:arrange_list(Varbinds) of
-				{ok, Pairs} ->
-					{ok, NamesValues} = snmp_collector_utils:oids_to_names(Pairs, []),
-					AlarmDetails = event(NamesValues),
-					{CommonEventHeader, FaultFields} = snmp_collector_utils:generate_maps(TargetName, AlarmDetails),
-					case snmp_collector_utils:log_events(CommonEventHeader, FaultFields) of
-						ok ->
-							ignore;
-						{error, Reason} ->
-							{error, Reason}
-					end;
+			{ok, Pairs} = snmp_collector_utils:arrange_list(Varbinds),
+			{ok, NamesValues} = snmp_collector_utils:oids_to_names(Pairs, []),
+			AlarmDetails = event(NamesValues),
+			{CommonEventHeader, FaultFields} = snmp_collector_utils:generate_maps(TargetName, AlarmDetails),
+			case snmp_collector_utils:log_events(CommonEventHeader, FaultFields) of
+				ok ->
+					ignore;
 				{error, Reason} ->
 					{error, Reason}
 			end
@@ -119,17 +115,13 @@ handle_trap(TargetName, {_Enteprise, _Generic, _Spec, _Timestamp, Varbinds}, _Us
 		true ->
 			ignore;
 		false ->
-			case snmp_collector_utils:arrange_list(Varbinds) of
-				{ok, Pairs} ->
-					{ok, NamesValues} = snmp_collector_utils:oids_to_names(Pairs, []),
-					AlarmDetails = event(NamesValues),
-					{CommonEventHeader, FaultFields} = snmp_collector_utils:generate_maps(TargetName, AlarmDetails),
-					case snmp_collector_utils:log_events(CommonEventHeader, FaultFields) of
-						ok ->
-							ignore;
-						{error, Reason} ->
-							{error, Reason}
-					end;
+			{ok, Pairs} = snmp_collector_utils:arrange_list(Varbinds),
+			{ok, NamesValues} = snmp_collector_utils:oids_to_names(Pairs, []),
+			AlarmDetails = event(NamesValues),
+			{CommonEventHeader, FaultFields} = snmp_collector_utils:generate_maps(TargetName, AlarmDetails),
+			case snmp_collector_utils:log_events(CommonEventHeader, FaultFields) of
+				ok ->
+					ignore;
 				{error, Reason} ->
 					{error, Reason}
 			end
