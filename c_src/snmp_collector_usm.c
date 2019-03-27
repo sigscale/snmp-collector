dfd5cc1a0eaceb3b4f02219e763af2e423d9651c
Vance Shipley <vances@sigscale.org>
2019-03-27 09:06:37 +0530

    calculate localized md5 key

--- c_src/snmp_collector_usm.c
+++ c_src/snmp_collector_usm.c
@@ -20,52 +20,73 @@
 ***/
 
 #include <openssl/evp.h>
+#include <string.h>
 #include "erl_nif.h"
 
 /* Password to Key Algorithm (MD5)
  * RFC2274 A.2.1.
  */
-static void
+int
 password_to_key_md5(uint8_t *password, int password_len,
-		uint8_t *engineID, int engineID_len, uint8_t *result, int result_len)
+		uint8_t *engine, int engine_len, uint8_t *key, int key_len)
 {
+	EVP_MD_CTX *context;
+	u_char *cp, *buf;
+	u_long index = 0;
+	u_long count = 0, i;
+
+	if (((context = (EVP_MD_CTX *) malloc(sizeof(EVP_MD_CTX))) == NULL)
+			|| ((buf = (u_char *) malloc(engine_len > 64 ? engine_len : 64)) == NULL))
+		return -1;
+	EVP_DigestInit(context, EVP_md5());
+	while (count < 1048576) {
+		cp = buf;
+		for (i = 0; i < 64; i++) {
+			*cp++ = password[index++ % password_len];
+		}
+		EVP_DigestUpdate(context, buf, 64);
+		count += 64;
+	}
+	EVP_DigestFinal(context, (unsigned char *) key, (unsigned int *) &key_len);
+	memcpy(buf, key, 16);
+	memcpy(buf + 16, engine, engine_len);
+	memcpy(buf + engine_len, key, 16);
+	EVP_DigestInit(context, EVP_md5());
+	EVP_DigestUpdate(context, buf, 32 + engine_len);
+	EVP_DigestFinal(context, (unsigned char *) key, (unsigned int *) &key_len);
+	free(context);
+	free(buf);
+	return 0;
 }
 
 /* Password to Key Algorithm (SHA)
  * RFC2274 A.2.2.
  */
-static void
+int
 password_to_key_sha(uint8_t *password, int password_len,
-		uint8_t *engineID, int engine_len, uint8_t *result, int result_len)
+		uint8_t *engine, int engine_len, uint8_t *key, int key_len)
 {
 }
 
 static ERL_NIF_TERM
 password_to_key_md5_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-	ErlNifBinary password, engineID, res;
+	ErlNifBinary password, engine, key;
 
 	if (!enif_inspect_iolist_as_binary(env, argv[0], &password)
-			|| !enif_inspect_iolist_as_binary(env, argv[1], &engineID))
+			|| !enif_inspect_iolist_as_binary(env, argv[1], &engine))
 		return enif_make_badarg(env);
-	if (!enif_alloc_binary(160, &res))
+	if (!enif_alloc_binary(160, &key))
 		return enif_raise_exception(env, enif_make_atom(env, "ealloc"));
-	password_to_key_md5(password.data, password.size, engineID.data, engineID.size, res.data, res.size);
-	return enif_make_binary(env, &res);
+	if (!password_to_key_md5(password.data, password.size,
+			engine.data, engine.size, key.data, key.size))
+		return enif_raise_exception(env, enif_make_atom(env, "ealloc"));
+	return enif_make_binary(env, &key);
 }
 
 static ERL_NIF_TERM
 password_to_key_sha_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-	ErlNifBinary password, engineID, res;
-
-	if (!enif_inspect_iolist_as_binary(env, argv[0], &password)
-			|| !enif_inspect_iolist_as_binary(env, argv[1], &engineID))
-		return enif_make_badarg(env);
-	if (!enif_alloc_binary(160, &res))
-		return enif_raise_exception(env, enif_make_atom(env, "ealloc"));
-	password_to_key_sha(password.data, password.size, engineID.data, engineID.size, res.data, res.size);
-	return enif_make_binary(env, &res);
 }
 
 static ErlNifFunc nif_funcs[] = {
