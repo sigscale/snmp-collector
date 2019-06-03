f904b38d9a5beac27d61248ccdb01e4acb85c8a9
Vance Shipley <vances@sigscale.org>
2019-06-03 08:29:00 +0000

    use 3gpp 32.111-2 event/alarm types

--- include/snmp_collector.hrl
+++ include/snmp_collector.hrl
@@ -5,24 +5,17 @@
 		authPass :: string() | undefined | '_',
 		privPass :: string() | undefined | '_'}).
 
-% Event Types [ITU-T X.733]
--define(ET_Power_System, "Power System").
+% Event Types [3GPP 32.111-2 Annex A]
+-define(ET_Communication_System, "Communications Alarm").
+-define(ET_Processing_Error, "Processing Error Alarm").
 -define(ET_Environmental_Alarm, "Environmental Alarm").
--define(ET_Signaling_System, "Signaling System").
--define(ET_Trunk_System, "Trunk System").
--define(ET_Hardware_System, "Hardware System").
--define(ET_Software_System, "Software System").
--define(ET_Running_System, "Running System").
--define(ET_Communication_System, "Communication System").
--define(ET_Quality_Of_Service_Alarm, "Quality Of ServiceAlarm").
--define(ET_Processing_Error, "Processing Error").
--define(ET_OMC, "OMC").
+-define(ET_Quality_Of_Service_Alarm, "Quality of Service Alarm").
+-define(ET_Equipment_Alarm, "Equipment Alarm").
 -define(ET_Integrity_Violation, "Integrity Violation").
 -define(ET_Operational_Violation, "Operational Violation").
 -define(ET_Physical_Violation, "Physical Violation").
 -define(ET_Security_Service_Or_Mechanism_Violation, "Security Service Or Mechanism Violation").
 -define(ET_Time_Domain_Violation, "Time Domain Violation").
--define(ET_Relay_System, "Relay System").
 
 % Probable Causes [3GPP 32.111-2 Annex B]
 -define(PC_Indeterminate, "Indeterminate").
