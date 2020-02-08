0318bc2c7ce73dbb9853f7f3de6c385a0391bde1
refath <refath@sigscale.com>
2020-02-08 14:19:51 +0000

    syntax

--- src/snmp_collector_event_log.erl
+++ src/snmp_collector_event_log.erl
@@ -74,15 +74,15 @@ init([] = _Args) ->
 %% %%    gen_event:notify/2, gen_event:sync_notify/2}.
 %% %% @private
 %%
-handle_event({CommonEventHeader, OtherFields}, State)
+handle_event({CommonEventHeader, OtherFields} = Event, State)
 		when is_map(CommonEventHeader), is_map(OtherFields) ->
 	case snmp_collector_log:fault_log(Event) of
 		ok ->
 			{ok, State};
 		{error, Reason} ->
 			error_logger:info_report(["SNMP Manager Logging Failed",
-				{error, Reason}]);
-		end
+				{error, Reason}])
+	end.
 
 
 -spec handle_call(Request, State) -> Result
