e7c5ead6cc9b336d1b66b8700c5ce78449d074e5
Vance Shipley <vances@sigscale.org>
2020-02-11 14:33:29 +0800

    only gather fault domain events

--- src/snmp_collector_event_ves.erl
+++ src/snmp_collector_event_ves.erl
@@ -202,7 +202,7 @@ code_change(_OldVsn, State, _Extra) ->
 gather(Events, State) ->
 	gather(Events, State, []).
 %% @hidden
-gather([{_, _, _, #{"reportingEntityId" := AgentId},
+gather([{_, _, _, #{"domain" := "fault", "reportingEntityId" := AgentId},
 		#{"alarmAdditionalInformation" := #{"alarmId" := AlarmId}}} | T],
 		#state{buffer = Buffer} = State, Acc) ->
 	F = fun({_, _, _, #{"reportingEntityId" := Agent},
@@ -214,7 +214,7 @@ gather([{_, _, _, #{"reportingEntityId" := AgentId},
 	end,
 	{Events, NewBuffer} = lists:partition(F, Buffer),
 	gather(T, State#state{buffer  = NewBuffer}, [lists:reverse(Events) | Acc]);
-gather([{_, _, _, #{"reportingEntityName" := AgentName},
+gather([{_, _, _, #{"domain" := "fault", "reportingEntityName" := AgentName},
 		#{"alarmAdditionalInformation" := #{"alarmId" := AlarmId}}} | T],
 		#state{buffer = Buffer} = State, Acc) ->
 	F = fun({_, _, _, #{"reportingEntityName" := Agent},
@@ -226,10 +226,12 @@ gather([{_, _, _, #{"reportingEntityName" := AgentName},
 	end,
 	{Events, NewBuffer} = lists:partition(F, Buffer),
 	gather(T, State#state{buffer  = NewBuffer}, [lists:reverse(Events) | Acc]);
+gather([H | T], State, Acc) ->
+	gather(T, State, [H | Acc]);
 gather([], State, Acc) ->
 	gather1(Acc, State, []).
 %% @hidden
-gather1([H | T], State, Acc) ->
+gather1([H | T], State, Acc) when is_list(H) ->
 	F = fun({_, _, _, #{"eventName" := notifyNewAlarm}, _}, _) ->
 				true;
 			({_, _, _, #{"eventName" := notifyChangedAlarm}, _},
@@ -239,6 +241,8 @@ gather1([H | T], State, Acc) ->
 				false
 	end,
 	gather1(T, State, [lists:sort(F, H) | Acc]);
+gather1([H | T], State, Acc) ->
+	gather1(T, State, [H | Acc]);
 gather1([], State, Acc) ->
 	{lists:flatten(Acc), State}.
 
