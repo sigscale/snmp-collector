e1b2a183e3db0d639f81f0969fa932d65ace903e
Vance Shipley <vances@sigscale.org>
2020-02-18 05:29:58 +0800

    Revert "fix dropping most recent event in gather"
    This reverts commit b10606e673334a7791a9845d59ce83d513701af1.

--- src/snmp_collector_event_ves.erl
+++ src/snmp_collector_event_ves.erl
@@ -204,7 +204,7 @@ gather(Events, State) ->
 	gather(Events, State, []).
 %% @hidden
 gather([{_, _, _, #{"domain" := "fault", "reportingEntityId" := AgentId},
-		#{"alarmAdditionalInformation" := #{"alarmId" := AlarmId}}} = Event | T],
+		#{"alarmAdditionalInformation" := #{"alarmId" := AlarmId}}} | T],
 		#state{buffer = Buffer} = State, Acc) ->
 	F = fun({_, _, _, #{"reportingEntityId" := Agent},
 					#{"alarmAdditionalInformation" := #{"alarmId" := ID}}})
@@ -214,10 +214,9 @@ gather([{_, _, _, #{"domain" := "fault", "reportingEntityId" := AgentId},
 				false
 	end,
 	{Events, NewBuffer} = lists:partition(F, Buffer),
-	gather(T, State#state{buffer  = NewBuffer},
-			[lists:reverse([Event | Events]) | Acc]);
+	gather(T, State#state{buffer  = NewBuffer}, [lists:reverse(Events) | Acc]);
 gather([{_, _, _, #{"domain" := "fault", "reportingEntityName" := AgentName},
-		#{"alarmAdditionalInformation" := #{"alarmId" := AlarmId}}} = Event | T],
+		#{"alarmAdditionalInformation" := #{"alarmId" := AlarmId}}} | T],
 		#state{buffer = Buffer} = State, Acc) ->
 	F = fun({_, _, _, #{"reportingEntityName" := Agent},
 					#{"alarmAdditionalInformation" := #{"alarmId" := ID}}})
@@ -227,8 +226,7 @@ gather([{_, _, _, #{"domain" := "fault", "reportingEntityName" := AgentName},
 				false
 	end,
 	{Events, NewBuffer} = lists:partition(F, Buffer),
-	gather(T, State#state{buffer  = NewBuffer},
-			[lists:reverse([Event | Events]) | Acc]);
+	gather(T, State#state{buffer  = NewBuffer}, [lists:reverse(Events) | Acc]);
 gather([H | T], State, Acc) ->
 	gather(T, State, [H | Acc]);
 gather([], State, Acc) ->
